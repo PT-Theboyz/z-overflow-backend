38412b84a9d037f3aff3f4d4ce124253ae33b4dc
faisal <faisalfirdani01@gmail.com>
2022-11-02 11:42:41 +0700

    Add documentation log in app.js

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -34,8 +34,8 @@ const sendResetPasswordEmail = async (to, token) => {
   // replace this url with the link to the reset password page of your front-end app
   const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
   const text = `Dear user,
-To reset your password, click on this link: ${resetPasswordUrl}
-If you did not request any password resets, then ignore this email.`;
+  To reset your password, click on this link: ${resetPasswordUrl}
+  If you did not request any password resets, then ignore this email.`;
   await sendEmail(to, subject, text);
 };
 
@@ -50,8 +50,8 @@ const sendVerificationEmail = async (to, token) => {
   // replace this url with the link to the email verification page of your front-end app
   const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
   const text = `Dear user,
-To verify your email, click on this link: ${verificationEmailUrl}
-If you did not create an account, then ignore this email.`;
+  To verify your email, click on this link: ${verificationEmailUrl}
+  If you did not create an account, then ignore this email.`;
   await sendEmail(to, subject, text);
 };
 
