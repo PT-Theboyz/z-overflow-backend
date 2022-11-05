c75be64c78f13a3f0c5c03fc0664fecf7d22e469
faisal <faisalfirdani01@gmail.com>
2022-11-05 16:43:05 +0700

    createQuestion in question controller

--- src/controllers/question.controller.js
+++ src/controllers/question.controller.js
@@ -1,4 +1,19 @@
 const httpStatus = require('http-status');
-const pick = require('../utils/pick');
-const ApiError = require('../utils/ApiError');
+// const pick = require('../utils/pick');
+// const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
+const { questionService } = require('../services');
+
+const createQuestion = catchAsync(async (req, res) => {
+    req.body = {
+        ...req.body,
+        author: req.user.id
+    }
+    const question = await questionService.createQuestion(req.body);
+    res.status(httpStatus.CREATED).send(question);
+});
+
+
+module.exports = {
+    createQuestion
+}
